acd853bd82b07351b72ea8e6c19fb32a71a93215
root <siraldo.bmx@gmail.com>
2018-11-14 18:21:16 +0000

    Convertendo para componentes funcionais Changes to be committed: 	modified:   src/App.js 	modified:   src/MovieCard.js 	modified:   src/MovieCardsList.js

--- src/MovieCardsList.js
+++ src/MovieCardsList.js
@@ -1,15 +1,8 @@
-import React, { Component } from 'react';
+import React from 'react';
 import MovieCard from './MovieCard';
 
-class MovieCardsList extends Component {
-  render() {
-    /*
-  Destructuring via ES6. We're getting the profiles, users, and movies properties
-  off of the pros passed into this presentational component. If you need a refresher on this syntax, check
-  out this course: https://www.udacity.com/course/es6-javascript-improved--ud356
-  */
-
-    const { profiles, users, movies } = this.props;
+const MovieCardsList = (props) => {
+    const { profiles, users, movies } = props;
     const usersByMovie = {};
 
     profiles.forEach(profile => {
@@ -30,12 +23,11 @@ class MovieCardsList extends Component {
         movieInfo={movies[id]}
       />
     ));
-
-    /*
-    Return JSX
-    */
-    return <ul>{movieCards}</ul>;
-  }
+	
+  return (<ul>{movieCards}</ul>
+    
+    )
 }
+
 
 export default MovieCardsList;
